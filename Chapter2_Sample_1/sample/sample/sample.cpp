aabadf78542e13be10e8a4e75b30be38bf6658ee
Hung Vu <dorepansua@gmail.com>
2025-07-13 22:36:10 +0700

    done chapter 3

--- Chapter2_Sample_1/sample/sample/sample.cpp
+++ Chapter2_Sample_1/sample/sample/sample.cpp
@@ -1,10 +1,38 @@
 #include <ntddk.h>
+
+#define DRIVER_TAG 'vDvH'
+
+UNICODE_STRING g_RegistryPath;
+
 void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) {
   UNREFERENCED_PARAMETER(DriverObject);
+
+  ExFreePool(g_RegistryPath.Buffer);
+  KdPrint(("Sample driver Unload called\n"));
 }
 extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,
                                 _In_ PUNICODE_STRING RegistryPath) {
   UNREFERENCED_PARAMETER(RegistryPath);
   DriverObject->DriverUnload = SampleUnload;
+
+#pragma warning(push)
+#pragma warning( \
+    disable : 4996)  // disable deprecated warning for ExAllocatePoolWithTag
+
+  g_RegistryPath.Buffer = (WCHAR*)ExAllocatePoolWithTag(
+      PagedPool, RegistryPath->Length, DRIVER_TAG);
+
+#pragma warning(pop)
+
+  if (g_RegistryPath.Buffer == nullptr) {
+    KdPrint(("Failed to allocate memory\n"));
+    return STATUS_INSUFFICIENT_RESOURCES;
+  }
+
+  g_RegistryPath.MaximumLength = RegistryPath->Length;
+  RtlCopyUnicodeString(&g_RegistryPath, (PCUNICODE_STRING)RegistryPath);
+  // %wZ is for UNICODE_STRING objects
+  KdPrint(("Copied registry path: %wZ\n", &g_RegistryPath));
+  //...
   return STATUS_SUCCESS;
 }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# WindowsKernelProgramming
+# Windows Kernel Programming Examples
+
+This repository contains hands-on examples and code samples from the book  
+**_Windows Kernel Programming_ by Pavel Yosifovich**.
+
+📚 **Topics covered** include:
+- Writing and building kernel-mode drivers
+- I/O handling and device interfaces
+- IRQL, synchronization, and memory management
+- Debugging and tracing with WinDbg and DebugView
+
+🧱 All examples are tested on Windows 10/11 with WDK version 26100 and Visual Studio 2022.
+
+> ⚠️ You’ll need the correct version of Windows SDK + WDK installed to build the drivers.
+
+---
+
+Feel free to explore and experiment!
