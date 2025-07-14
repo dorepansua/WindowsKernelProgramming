ba614ec4de49808df18ee07a7d238ae0032c6a9d
Hung Vu <dorepansua@gmail.com>
2025-07-14 23:23:23 +0700

    step 1 init driver

---
+++ Chapter4_Sample1/PriorityBooster/PriorityBooster/PriorityBooster.cpp
@@ -0,0 +1,21 @@
+#include <ntddk.h>
+
+// prototypes
+void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject);
+
+NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject,
+                                    _In_ PIRP Irp);
+
+// DriverEntry
+extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,
+                                _In_ PUNICODE_STRING RegistryPath) {
+  DriverObject->DriverUnload = PriorityBoosterUnload;
+
+  DriverObject->MajorFunction[IRP_MJ_CREATE] = PriorityBoosterCreateClose;
+  DriverObject->MajorFunction[IRP_MJ_CLOSE] = PriorityBoosterCreateClose;
+  DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] =
+      PriorityBoosterDeviceControl;
+
+  return STATUS_SUCCESS;
+}
+void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {}
