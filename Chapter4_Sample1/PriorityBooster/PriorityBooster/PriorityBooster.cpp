5ede18bf3301c1866aacfb84aa3dbfb371d83137
Hung Vu <dorepansua@gmail.com>
2025-07-16 23:20:22 +0700

    add client code

--- Chapter4_Sample1/PriorityBooster/PriorityBooster/PriorityBooster.cpp
+++ Chapter4_Sample1/PriorityBooster/PriorityBooster/PriorityBooster.cpp
@@ -2,9 +2,10 @@
 
 // prototypes
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject);
-
 NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject,
                                     _In_ PIRP Irp);
+NTSTATUS PriorityBoosterDeviceControl(_In_ PDEVICE_OBJECT DeviceObject,
+                                      _In_ PIRP Irp);
 
 // DriverEntry
 extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,
@@ -16,6 +17,41 @@ extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,
   DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] =
       PriorityBoosterDeviceControl;
 
+  UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\PriorityBooster");
+  // RtlInitUnicodeString(&devName, L"\\Device\\ThreadBoost");
+
+  PDEVICE_OBJECT DeviceObject;
+
+  UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\PriorityBooster");
+  // RtlInitUnicodeString(&devName, L"\\Device\\ThreadBoost");
+  PDEVICE_OBJECT DeviceObject;
+  NTSTATUS status = IoCreateDevice(
+      DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+  if (!NT_SUCCESS(status)) {
+    KdPrint(("Failed to create device (0x%08X)\n", status));
+    return status;
+  }
+
+  UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+  status = IoCreateSymbolicLink(&symLink, &devName);
+  if (!NT_SUCCESS(status)) {
+    KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+    IoDeleteDevice(DeviceObject);
+    return status;
+  }
+
+  KdPrint(("PriorityBooster DriverEntry completed successfully\n"));
+
   return STATUS_SUCCESS;
 }
-void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {}
+
+void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
+  UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+  // delete symbolic link
+  IoDeleteSymbolicLink(&symLink);
+
+  // delete device object
+  IoDeleteDevice(DriverObject->DeviceObject);
+
+  KdPrint(("PriorityBooster unloaded\n"));
+}
